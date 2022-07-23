b8622e59e182f0fbd649016a4fa992aecc44737d
sudhir <sudhir9297@gmail.com>
2022-07-23 13:24:50 +0530

    Added Necessary dependencies

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
+import { HomePage } from "./pages";
 
 function App() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <h1>Strange Attractor</h1>
+    <div>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homePage/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      className="text-9xl font-bold"
+    >
+      Strange Attractor
+    </div>
+  );
+}
+
+export default HomePage;
